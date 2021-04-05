3507552720d7ad19bb17664c64161cbf61c6eb46
Tammy <tammy.n.tran@gmail.com>
2021-04-05 16:14:40 -0400

    inital commit

---
+++ App.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native'; //for iOS/Android
+import {Button} from 'react-native-elements'; //3rd party pkg allows display different comps
+
+// to be able to use react-native-elements, you need to configure by npm installing then npx linking
